0d101d92577071089e3524187b17185271d08583
Colton Thompson <colton.thompson@live.com>
2021-08-07 01:57:57 -0600

    Update App.js
    Added Container/Row/Col classes to the application.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import logo from './logo.svg';
 import React from 'react';
 import './App.css';
+import { Row } from 'react-bootstrap';
+import { Container } from 'react-bootstrap';
+import { Col } from 'react-bootstrap';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Leaderboard from './components/Leaderboard';
@@ -9,12 +12,18 @@ import PointsBreakdown from './components/PointsBreakdown';
 function App() {
   return (
     <React.Fragment>
-      <div class='container'>
-        <Header />
-        <Leaderboard></Leaderboard>
-        <PointsBreakdown />
-        <Footer />
-      </div>
+      <Container>
+        <Row>
+          <Col xs lg='2'></Col>
+          <Col md='auto'>
+            <Header />
+            <Leaderboard></Leaderboard>
+            <PointsBreakdown />
+            <Footer />
+          </Col>
+          <Col xs lg='2'></Col>
+        </Row>
+      </Container>
     </React.Fragment>
   );
 }
